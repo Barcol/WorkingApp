a36560372469c44033dfdb3268a46c4ffd74f5f6
barcol <barcol1@interia.pl>
2019-03-21 20:16:07 +0100

    available exercises list added

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -12,11 +14,12 @@ public class Main {
     public static void main(String[] args) {
         ExerciseGenerator execGen = new ExerciseGenerator();
         JSONParser jsonParser = new JSONParser();
+        ArrayList availableExercises = new ArrayList();
         try (FileReader reader = new FileReader("exercises.json"))
         {
             Object obj = jsonParser.parse(reader);
             JSONArray exercisesList = (JSONArray) obj;
-            exercisesList.forEach( exec -> execGen.generateExercise( (JSONObject) exec ) );
+            exercisesList.forEach( exec -> availableExercises.add(execGen.generateExercise((JSONObject) exec )));
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
