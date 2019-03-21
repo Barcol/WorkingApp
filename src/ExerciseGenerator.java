1af8b233e14bcca1afacce3acea530869d1d286f
barcol <barcol1@interia.pl>
2019-03-21 20:55:37 +0100

    small error repaired

--- src/ExerciseGenerator.java
+++ src/ExerciseGenerator.java
@@ -3,7 +3,7 @@ import java.util.Map;
 public class ExerciseGenerator {
 
     public SimpleExercise generateExercise(Map data) {
-        return new SimpleExercise((String) data.get("name"), (int) data.get("reps"), (int) data.get("series"),
-                (char) data.get("week"));
+        return new SimpleExercise((String) data.get("name"), Math.toIntExact((long) data.get("reps")),
+                Math.toIntExact((long) data.get("series")), ((String) data.get("week")).charAt(0));
     }
 }
